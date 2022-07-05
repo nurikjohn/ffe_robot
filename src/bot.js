35aeb14d2e06dec0f6c9d134b9de42ec980a8953
Nuriddin <nmuhammadjanov@gmail.com>
2022-07-05 20:29:16 +0500

    bug fix

--- src/bot.js
+++ src/bot.js
@@ -56,57 +56,69 @@ const getRandomQuestion = async (
 };
 
 bot.start(async (ctx) => {
-    const user = ctx.session.user;
+    try {
+        const user = ctx.session.user;
 
-    const subjects = await Subject.find({
-        $or: [
-            {
-                public: true,
-            },
-            {
-                allowed_users: {
-                    $in: [user._id],
+        const subjects = await Subject.find({
+            $or: [
+                {
+                    public: true,
                 },
-            },
-        ],
-    });
+                {
+                    allowed_users: {
+                        $in: [user._id],
+                    },
+                },
+            ],
+        });
 
-    ctx.reply(
-        `Fan tanlang
+        ctx.reply(
+            `Fan tanlang
 
 O'quv yili: 2021-2022
 Kurs: 3`,
-        Markup.inlineKeyboard(
-            subjects.map((subject) => [
-                {
-                    text: subject.name,
-                    callback_data: `subject_${subject._id}`,
-                },
-            ])
-        )
-    );
+            Markup.inlineKeyboard(
+                subjects.map((subject) => [
+                    {
+                        text: subject.name,
+                        callback_data: `subject_${subject._id}`,
+                    },
+                ])
+            )
+        );
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 bot.command("stop", async (ctx) => {
-    const user = ctx.session.user;
+    try {
+        const user = ctx.session.user;
 
-    await Session.updateMany(
-        {
-            user: user._id,
-            is_completed: false,
-        },
-        {
-            is_completed: true,
-        }
-    );
+        await Session.updateMany(
+            {
+                user: user._id,
+                is_completed: false,
+            },
+            {
+                is_completed: true,
+            }
+        );
 
-    ctx.reply(`Boshlangan barcha testlar yakunlandi.
+        ctx.reply(`Boshlangan barcha testlar yakunlandi.
 
 Qaytadan test ishlash uchun /start ni bosing.`);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 bot.action(/subject_(.+)/, async (ctx) => {
-    ctx.answerCbQuery();
+    try {
+        ctx.answerCbQuery();
+    } catch (error) {
+        console.log(error);
+    }
 
     const subjectId = ctx.match?.[1];
 
@@ -128,165 +140,46 @@ Ajratilgan vaqt: 60 daqiqa`,
 });
 
 bot.action(/start_(.+)/, async (ctx) => {
-    const user = ctx.session.user;
+    try {
+        const user = ctx.session.user;
 
-    const subjectId = ctx.match?.[1];
+        const subjectId = ctx.match?.[1];
 
-    let session = await Session.findOne({
-        user: user._id,
-        is_completed: false,
-    });
+        let session = await Session.findOne({
+            user: user._id,
+            is_completed: false,
+        });
 
-    if (session) {
-        return ctx.reply(
-            `Sizda oldinroq boshlangan test mavjud, testni yakunlagach qaytadan boshlashingiz mumkun!
+        if (session) {
+            return ctx.reply(
+                `Sizda oldinroq boshlangan test mavjud, testni yakunlagach qaytadan boshlashingiz mumkun!
 
 Oldinroq boshlangan testlarni yakunlash uchun /stop buyrug'ini yuborishingiz mumkun`
-        );
-    }
-
-    const question = await getRandomQuestion([], subjectId);
-
-    if (question) {
-        ctx.answerCbQuery();
-        ctx.editMessageReplyMarkup();
-
-        const options = _.shuffle(question.options);
-        const correctOptionId = options.indexOf(
-            question.options[question.correct_option_id]
-        );
-
-        const quiz = await ctx.replyWithQuiz(
-            `1. (${question.difficulty} ball) ${question.question}`,
-            options.map((option) => option.slice(0, 100)),
-            {
-                correct_option_id: correctOptionId,
-                is_anonymous: false,
-            }
-        );
-
-        await Session.create({
-            user: user._id,
-            subject: subjectId,
-            questions: [
-                {
-                    question: question._id,
-                    telegram_poll_id: quiz.poll.id,
-                    correct_option_id: correctOptionId,
-                },
-            ],
-        });
-    } else {
-        ctx.answerCbQuery(
-            `Kechirasiz, afsuski bu fanga tegishli savollar topilmadi :(`,
-            { show_alert: true }
-        );
-    }
-});
-
-bot.on("poll_answer", async (ctx) => {
-    const user = ctx.session.user;
-    const poll = ctx.pollAnswer;
-
-    let session = await Session.findOne({
-        user: user._id,
-        is_completed: false,
-    }).populate("questions.question");
-
-    if (!session) return null;
-
-    const question = session.questions.find(
-        ({ telegram_poll_id }) => telegram_poll_id == poll.poll_id
-    );
-
-    if (question) {
-        const isCorrectAnswer =
-            question.correct_option_id == poll.option_ids[0];
-
-        session = await Session.findOneAndUpdate(
-            {
-                _id: session._id,
-                "questions._id": question._id,
-            },
-            {
-                $set: {
-                    "questions.$.correct": isCorrectAnswer,
-                },
-            },
-            {
-                new: true,
-            }
-        ).populate("subject questions.question");
-
-        if (session.questions.length >= QUESTIONS_COUNT) {
-            const correctAnswersCount = session.questions?.filter(
-                ({ correct }) => correct
-            ).length;
-
-            const incorrectAnswersCount =
-                session.questions.length - correctAnswersCount;
-
-            const ball = session.questions?.reduce((summ, question) => {
-                if (question.correct) {
-                    return summ + question.question.difficulty;
-                }
-
-                return summ;
-            }, 0);
-
-            const percent = ((ball / MAX_BALL) * 100).toFixed(2);
-
-            const startTime = dayjs(session.created_at);
-            const endTime = dayjs(session.updated_at);
-
-            const mins = endTime.diff(startTime, "minutes", true);
-            const hours = parseInt(mins / 60)
-                .toString()
-                .padStart(2, "0");
-            const minutes = dayjs().minute(mins).$m.toString().padStart(2, "0");
-
-            ctx.telegram.sendMessage(
-                user.telegram_user_id,
-                `Fan: ${session.subject.name}
-Ball: ${ball} (${percent}%)
-To'g'ri javoblar: ${correctAnswersCount}
-Xato javoblar: ${incorrectAnswersCount}
-
-Sarflangan vaqt: ${hours}:${minutes}`,
-                Markup.inlineKeyboard([
-                    [
-                        {
-                            text: "Qayta urinib ko'rish",
-                            callback_data: `start_${session.subject._id}`,
-                        },
-                    ],
-                ])
             );
+        }
 
-            await Session.findByIdAndUpdate(session._id, {
-                is_completed: true,
-            });
-        } else {
-            const nextQuestion = await getRandomQuestion(
-                session.questions?.map(({ question }) => question._id),
-                session.subject._id,
-                session.questions.length >= 20
-                    ? 3
-                    : session.questions.length >= 15
-                    ? 1
-                    : 2
-            );
+        const question = await getRandomQuestion([], subjectId);
 
-            const options = _.shuffle(nextQuestion.options);
+        if (question) {
+            try {
+                ctx.answerCbQuery();
+            } catch (error) {
+                console.log(error);
+            }
+
+            try {
+                ctx.editMessageReplyMarkup();
+            } catch (error) {
+                console.log(error);
+            }
+
+            const options = _.shuffle(question.options);
             const correctOptionId = options.indexOf(
-                nextQuestion.options[nextQuestion.correct_option_id]
+                question.options[question.correct_option_id]
             );
 
-            const quiz = await ctx.telegram.sendQuiz(
-                user.telegram_user_id,
-                `${session.questions?.length + 1}. (${
-                    nextQuestion.difficulty
-                } ball) ${nextQuestion.question}`,
+            const quiz = await ctx.replyWithQuiz(
+                `1. (${question.difficulty} ball) ${question.question}`,
                 options.map((option) => option.slice(0, 100)),
                 {
                     correct_option_id: correctOptionId,
@@ -294,16 +187,155 @@ Sarflangan vaqt: ${hours}:${minutes}`,
                 }
             );
 
-            await Session.findByIdAndUpdate(session._id, {
-                $push: {
-                    questions: {
-                        question: nextQuestion._id,
+            await Session.create({
+                user: user._id,
+                subject: subjectId,
+                questions: [
+                    {
+                        question: question._id,
                         telegram_poll_id: quiz.poll.id,
                         correct_option_id: correctOptionId,
                     },
-                },
+                ],
             });
+        } else {
+            ctx.answerCbQuery(
+                `Kechirasiz, afsuski bu fanga tegishli savollar topilmadi :(`,
+                { show_alert: true }
+            );
         }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+bot.on("poll_answer", async (ctx) => {
+    try {
+        const user = ctx.session.user;
+        const poll = ctx.pollAnswer;
+
+        let session = await Session.findOne({
+            user: user._id,
+            is_completed: false,
+        }).populate("questions.question");
+
+        if (!session) return null;
+
+        const question = session.questions.find(
+            ({ telegram_poll_id }) => telegram_poll_id == poll.poll_id
+        );
+
+        if (question) {
+            const isCorrectAnswer =
+                question.correct_option_id == poll.option_ids[0];
+
+            session = await Session.findOneAndUpdate(
+                {
+                    _id: session._id,
+                    "questions._id": question._id,
+                },
+                {
+                    $set: {
+                        "questions.$.correct": isCorrectAnswer,
+                    },
+                },
+                {
+                    new: true,
+                }
+            ).populate("subject questions.question");
+
+            if (session.questions.length >= QUESTIONS_COUNT) {
+                const correctAnswersCount = session.questions?.filter(
+                    ({ correct }) => correct
+                ).length;
+
+                const incorrectAnswersCount =
+                    session.questions.length - correctAnswersCount;
+
+                const ball = session.questions?.reduce((summ, question) => {
+                    if (question.correct) {
+                        return summ + question.question.difficulty;
+                    }
+
+                    return summ;
+                }, 0);
+
+                const percent = ((ball / MAX_BALL) * 100).toFixed(2);
+
+                const startTime = dayjs(session.created_at);
+                const endTime = dayjs(session.updated_at);
+
+                const mins = endTime.diff(startTime, "minutes", true);
+                const hours = parseInt(mins / 60)
+                    .toString()
+                    .padStart(2, "0");
+                const minutes = dayjs()
+                    .minute(mins)
+                    .$m.toString()
+                    .padStart(2, "0");
+
+                ctx.telegram.sendMessage(
+                    user.telegram_user_id,
+                    `Fan: ${session.subject.name}
+Ball: ${ball} (${percent}%)
+To'g'ri javoblar: ${correctAnswersCount}
+Xato javoblar: ${incorrectAnswersCount}
+
+Sarflangan vaqt: ${hours}:${minutes}`,
+                    Markup.inlineKeyboard([
+                        [
+                            {
+                                text: "Qayta urinib ko'rish",
+                                callback_data: `start_${session.subject._id}`,
+                            },
+                        ],
+                    ])
+                );
+
+                await Session.findByIdAndUpdate(session._id, {
+                    is_completed: true,
+                });
+            } else {
+                const nextQuestion = await getRandomQuestion(
+                    session.questions?.map(({ question }) => question._id),
+                    session.subject._id,
+                    session.questions.length >= 20
+                        ? 3
+                        : session.questions.length >= 15
+                        ? 1
+                        : 2
+                );
+
+                const options = _.shuffle(nextQuestion.options);
+                const correctOptionId = options.indexOf(
+                    nextQuestion.options[nextQuestion.correct_option_id]
+                );
+
+                const quiz = await ctx.telegram.sendQuiz(
+                    user.telegram_user_id,
+                    `${session.questions?.length + 1}. (${
+                        nextQuestion.difficulty
+                    } ball) ${nextQuestion.question}`,
+                    options.map((option) => option.slice(0, 100)),
+                    {
+                        correct_option_id: correctOptionId,
+                        is_anonymous: false,
+                    }
+                );
+
+                await Session.findByIdAndUpdate(session._id, {
+                    $push: {
+                        questions: {
+                            question: nextQuestion._id,
+                            telegram_poll_id: quiz.poll.id,
+                            correct_option_id: correctOptionId,
+                        },
+                    },
+                });
+            }
+        }
+    } catch (error) {
+        console.log(error);
     }
 });
 
@@ -312,19 +344,21 @@ bot.command("/broadcast", async (ctx) => {
 
     // if (user.telegram_user_id == 370269361) {
     //     const users = await User.find();
-    //     users.map((user) => {
+
+    //     for (const item of users) {
+    //         console.log(item);
     //         try {
-    //             ctx.telegram.sendMessage(
-    //                 user.telegram_user_id,
+    //             await ctx.telegram.sendMessage(
+    //                 item.telegram_user_id,
     //                 `⚡️⚡️⚡️
 
-    // ***Mobil ilovalarni ishlab chiqish***, ***Makroiqtisodiyot*** fanlari botga qo'shildi.`,
+    //     ***Mobil ilovalarni ishlab chiqish***, ***Makroiqtisodiyot*** fanlari botga qo'shildi.`,
     //                 { parse_mode: "Markdown" }
     //             );
     //         } catch (error) {
     //             console.log(error);
     //         }
-    //     });
+    //     }
     // }
 });
 
